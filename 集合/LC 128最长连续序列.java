28efeb92b7ba8f61f9ab10befbf8f4a9530389ea
Mongxin <1393907820@qq.com>
2025-12-06 16:29:10 +0800

    Update LC 128最长连续序列

---
+++ 集合/LC 128最长连续序列.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> hashSet= new HashSet<Integer>();
+        for(int data : nums) hashSet.add(data);
+        int answer = 0;
+
+        for(int x : nums){
+            if(hashSet.contains(x-1)){//注意不是containsKey
+                continue;
+            }
+            int y = x + 1;
+            while (hashSet.contains(y)) {
+                y++;
+            }
+            ans = Math.max(ans, y - x);
+        }
+        return ans;
+    }
+}
