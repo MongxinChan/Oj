52faaf79e02a3a6e9a98603b3a7cc6744be68160
Mongxin <1393907820@qq.com>
2025-12-08 23:16:53 +0800

    Update LC 11盛水最多的容器

---
+++ 双指针/LC 11盛水最多的容器.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int ans = 0 , left = 0 ,right = height.length-1;
+        while (left<right){
+            int area = (right - left)*Math.min(height[left],height[right]);
+            ans = Math.max(ans,area);
+            if(height[left]<height[right]){
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return ans;
+    }
+}
